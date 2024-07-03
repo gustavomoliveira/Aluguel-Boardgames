e8b75a4dac3396f275b5aa7d3545da3e63c15419
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-07-03 12:46:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,11 +2,14 @@ function alterarStatus(id) {
     let jogoClicado = document.getElementById(`game-${id}`);
     let imagemJogo = jogoClicado.querySelector(".dashboard__item__img");
     let botao = jogoClicado.querySelector(".dashboard__item__button");
+    let nomeJogo = jogoClicado.querySelector(".dashboard__item__name");
    
     if(imagemJogo.classList.contains("dashboard__item__img--rented")) {
-        imagemJogo.classList.remove("dashboard__item__img--rented");
-        botao.classList.remove("dashboard__item__button--return");
-        botao.innerHTML = "Alugar";
+        if(confirm(`Você deseja retornar o jogo ${nomeJogo.innerHTML}?`)) {
+            imagemJogo.classList.remove("dashboard__item__img--rented");
+            botao.classList.remove("dashboard__item__button--return");
+            botao.innerHTML = "Alugar";
+        }
     } else {
         imagemJogo.classList.add("dashboard__item__img--rented");
         botao.classList.add("dashboard__item__button--return");
