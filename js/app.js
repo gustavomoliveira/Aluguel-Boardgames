13c373553931fa85d409f90102b779fa1831dcd9
Gustavo Mendes de Oliveira <167227958+gustavomoliveira@users.noreply.github.com>
2024-07-03 12:21:49 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-function adicionar() {
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
-
-    alert(nomeProduto.value);
-    alert(quantidade.value);
+function alterarStatus(id) {
+    let jogoClicado = document.getElementById(`game-${id}`);
+    let imagemJogo = jogoClicado.querySelector(".dashboard__item__img");
+    let botao = jogoClicado.querySelector(".dashboard__item__button");
+   
+    if(imagemJogo.classList.contains("dashboard__item__img--rented")) {
+        imagemJogo.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.innerHTML = "Alugar";
+    } else {
+        imagemJogo.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.innerHTML = "Devolver";
+    }
 }
